6c77183d6afeda9d047af8460e127d44898d59e2
kusaljr <prastutlamsal41@gmail.com>
2021-06-08 14:13:45 +0545

    ui started

--- route/index.js
+++ route/index.js
@@ -23,10 +23,17 @@ router.get('/rank', auth, (req, res)=>{
     .then(res => res.json())
     .then(rank => {
         res.render('components/rank', {rank: rank})
-    });
-
-    
+    }); 
 })
+
+router.get('/fantasy', auth, (req, res)=>{
+    fetch('http://localhost:4000/api/Players/Spain/Portugal')
+    .then(res => res.json())
+    .then(players => {
+        res.render('components/fantasy', {players: players})
+    }); 
+})
+
 
 
 module.exports = router;
